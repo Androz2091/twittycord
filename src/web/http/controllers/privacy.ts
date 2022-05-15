85431cd7014a82df03ced7a38c1dd9228277c49b
Androz <androz2091@gmail.com>
2022-05-15 10:18:16 +0200

    :sparkles: Add privacy page

---
+++ src/web/http/controllers/privacy.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express"
+
+export default {
+    index: (req: Request, res: Response, next: NextFunction) => {
+        res.render('privacy');
+    }
+}
